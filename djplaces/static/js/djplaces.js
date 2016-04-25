e130009deb5af19ae8de099dc38d81ae076f12f7
Oscar Cortez <om.cortez.2010@gmail.com>
2016-04-25 15:54:48 -0600

    Update lat and lng on marker dragged

--- djplaces/static/js/djplaces.js
+++ djplaces/static/js/djplaces.js
@@ -3,22 +3,20 @@
     $("#id_place")
       .geocomplete({
         map: "#map_location",
-        mapOptions: {
-          zoom: 10
-        },
-        markerOptions: {
-          draggable: true
-        }
+        mapOptions: { zoom: 10 },
+        markerOptions: { draggable: true }
       })
       .bind("geocode:result", function(event, result) {
-        var coordinates = result.geometry.location.lat() + ',' + result.geometry.location.lng();
-        $('#id_location').val(coordinates);
+        $('#id_location').val(result.geometry.location.lat() + ',' + result.geometry.location.lng());
       })
       .bind("geocode:error", function(event, status){
         console.log("ERROR: " + status);
       })
       .bind("geocode:multiple", function(event, results){
         console.log("Multiple: " + results.length + " results found");
+      })
+      .bind("geocode:dragged", function(event, latLng){
+        $('#id_location').val(latLng.lat() + ',' + latLng.lng());
       });
   });
 
