037632600c80ee3f4734809da5950097819ce999
Oscar Cortez <om.cortez.2010@gmail.com>
2016-04-28 10:27:18 -0600

    Js fixes, add support for stored locations

--- djplaces/static/js/djplaces.js
+++ djplaces/static/js/djplaces.js
@@ -1,22 +1,31 @@
 
-  $(document).ready(function() {
-    $("#id_place")
-      .geocomplete({
+$(document).ready(function() {
+
+  var options = {
         map: "#map_location",
         mapOptions: { zoom: 10 },
         markerOptions: { draggable: true }
-      })
-      .bind("geocode:result", function(event, result) {
-        $('#id_location').val(result.geometry.location.lat() + ',' + result.geometry.location.lng());
-      })
-      .bind("geocode:error", function(event, status){
-        console.log("ERROR: " + status);
-      })
-      .bind("geocode:multiple", function(event, results){
-        console.log("Multiple: " + results.length + " results found");
-      })
-      .bind("geocode:dragged", function(event, latLng){
-        $('#id_location').val(latLng.lat() + ',' + latLng.lng());
-      });
-  });
+      },
+      $geocomplete = $("#id_place");
+
+  if ( $('#id_location').val() ) {
+    options.location = $('#id_location').val()
+  }
+
+  $geocomplete
+    .geocomplete(options)
+    .bind("geocode:result", function(event, result) {
+      $('#id_location').val(result.geometry.location.lat() + ',' + result.geometry.location.lng());
+    })
+    .bind("geocode:error", function(event, status){
+      console.log("ERROR: " + status);
+    })
+    .bind("geocode:multiple", function(event, results){
+      console.log("Multiple: " + results.length + " results found");
+    })
+    .bind("geocode:dragged", function(event, latLng){
+      $('#id_location').val(latLng.lat() + ',' + latLng.lng());
+    });
+
+});
 
