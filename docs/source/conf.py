3dfee258f94c7f08059a49d89a278ff22227785a
Oscar Cortez <om.cortez.2010@gmail.com>
2022-09-01 11:46:37 -0500

    Add github pages action

---
+++ docs/source/conf.py
@@ -0,0 +1,16 @@
+project = ''
+copyright = '2022, Oscar Cortez'
+author = 'Oscar Cortez'
+release = ''
+
+extensions = [
+    'sphinx_mdinclude',
+]
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
+html_theme = 'furo'
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-========
-Usage
-========
-
-To use dj-places in a project::
-
-    import djplaces
